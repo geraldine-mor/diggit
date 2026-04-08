6dc88293dac6c93cd850a65505e44fb427149b5d
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-04-08 16:10:58 +0100

    Add Post model, migrate and test in the admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))    
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="posts" 
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=210, unique=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
