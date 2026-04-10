6cf39944d692690d5ef8e992854afd99544b26b5
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-04-10 11:33:55 +0100

    Add filtering so only published posts display

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,7 @@ from .models import Post
 
 # Create your views here.
 class PostList (generic.ListView):
-    queryset = Post.objects.all()
+    # Need to filter this to blog 
+    # posts only when Forum posts are an option
+    queryset = Post.objects.filter(status=1)
     template_name = "digging_deeper.html"
