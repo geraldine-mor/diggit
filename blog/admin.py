b66d9a680a41036f0dc42db3c9bf42c384f0d77b
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-04-13 16:29:01 +0100

    Exclude excerpt from admin form and display excerpt instead of content

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'created_on')
     search_fields = ('title', 'content')
     list_filter = ('status',)
+    exclude = ['excerpt',]
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
