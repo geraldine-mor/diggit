fffdd16fed5c50e171e60caeaf70e5d49d0962f4
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-04-21 10:45:38 +0100

    Prepare for deployment

---
+++ staticfiles/js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    
+    $("#comment-btn").click(function(){
+        $("#comment-form").show();
+    });
+
+    $("#comment-close").click(function(){
+        $("#comment-form").hide();
+    });
+
+    $("#post-form-cancel").click(function(){
+        $("#post-form>form")[0].reset();
+    });
+
+    const editButtons = document.getElementsByClassName("post-edit-btn");
+    if (editButtons.length > 0) {
+        postEdit(editButtons);
+    };
+
+    const deleteButtons = document.getElementsByClassName("post-delete-btn");
+    if (deleteButtons.length > 0) {
+        postDelete(deleteButtons);
+    };
+
+});
+
+function postEdit(editButtons){
+    const postTitle = document.getElementById("id_title");
+    const postContent = document.getElementById("id_content");
+    const postSave = document.getElementById("post-save");
+    const postForm = document.getElementById("edit-create-post");
+
+    for (let button of editButtons) {
+        button.addEventListener("click", (e) =>{
+            let postSlug = e.target.getAttribute("data-post-slug");
+            postTitle.value = e.target.getAttribute("data-title");
+            postContent.value = e.target.getAttribute("data-content");
+            postSave.innerText = "Update";
+            postForm.setAttribute("action", `/${postSlug}/edit_post/`);
+        });
+    };
+}
+
+function postDelete(deleteButtons) {
+    const postDelete = document.getElementById("confirm-post-delete");
+
+    for (let button of deleteButtons) {
+        button.addEventListener("click", (e) => {
+            let postSlug = e.target.getAttribute("data-post-slug");
+            postDelete.setAttribute("href", `/${postSlug}/delete_post/`);
+        });
+    };
+}
