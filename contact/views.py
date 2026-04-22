7b25c35a215744fb3fb4a06530af486af12b1958
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-04-22 11:21:02 +0100

    Add contact form and view, test that it works

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from .forms import MessageForm
 
 # Create your views here.
 def contact(request):
-    return render(request, 'contact/contact.html')
+    """
+    Display a contact form for :model: `contact.Message`
+
+    **Context**
+    ``contact_form`` 
+        The form to send a message to the site admin   
+
+    **Template**
+    :template: contact/contact.html
+    """
+
+    if request.method == "POST":
+        contact_form = MessageForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(request, messages.SUCCESS, 
+                                 'Your message has been sent')
+            
+
+    contact_form = MessageForm()
+
+    return render(
+        request,
+        "contact/contact.html",
+        { "contact_form": contact_form }
+    )
