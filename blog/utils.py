63b3a6bf07540cd6bfd5c633a6550b4a09d1e262
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-05-04 10:47:14 +0100

    Add comments to blog/utils.py

--- blog/utils.py
+++ blog/utils.py
@@ -1,8 +1,19 @@
+"""Helper functions for diggit/blog"""
+
 def excerpt_generator(content):
+    """
+    Creates a 30 word excerpt for display on preview post lists
+
+    Args:
+        content (string): content field of :model:`Post`
+
+    Returns:
+        string: first 30 words of longer posts appended with ... or
+        content where the length is <30 words
+    """
     excerpt_list = content.split()[:30]
-    if (len(excerpt_list) >= 30):  
+    if (len(excerpt_list) >= 30):
         excerpt = " ".join(excerpt_list)
         return excerpt + "..."
     else:
         return content
-
