bc2adb6835dc39a1c2523b860e3aa6f5a37b5544
Geraldine <geraldine@rocketmal.com>
2026-05-08 18:56:23 +0100

    Add a post form test

--- blog/tests_forms.py
+++ blog/tests_forms.py
@@ -1,17 +1,26 @@
 from django.test import TestCase
 from .forms import PostForm, CommentForm
+from .models import Category
 
 
 class TestPostForm(TestCase):
+
+    def setUp(self):
+        self.categories = Category(
+            name="Miscellaneous",
+            label_colour="#000000" 
+        )
+        self.categories.save()
 
     def test_post_form_is_valid(self):
         """
         Test that the form is valid when title,
         content and category fields are completed
         """
+        
         post_form = PostForm(
             {'title': 'Post Title',
              'content': 'Post content',
-             'categories': []}
+             'categories': [1]}
         )
-        self.assertTrue(post_form.is_valid(), post_form.errors)
+        self.assertTrue(post_form.is_valid())
