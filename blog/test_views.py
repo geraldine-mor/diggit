009def65f2cfc4bea7bd7c82d049cbb7be3f744f
Geraldine <geraldine@rocketmal.com>
2026-05-09 10:05:27 +0100

    Add test to check that guests can't create posts and that users can edit own posts

--- blog/test_views.py
+++ blog/test_views.py
@@ -90,6 +90,23 @@ class TestForumListView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Post created", response.content)
 
+    def test_logged_out_user_cannot_post(self):
+        """
+        Test that logged out users can't create a post
+        """
+        
+        data = {
+            'title': 'Logged Out Test',
+            'content': 'Test user generated content',
+            'categories': [1]
+        }
+        response = self.client.post(
+            "/diggit_forum/", data)
+       
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(
+            Post.objects.filter(title="Logged Out Test").exists())
+
     
 class TestHomePageView(TestCase):
 
@@ -157,3 +174,62 @@ class TestReadPostView(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Comment saved", response.content)
+
+
+class TestEditPostView(TestCase):
+
+    def setUp(self):
+        self.user_a = User.objects.create_user(
+            first_name="John",
+            last_name="Smith",
+            email="john@smith.com",
+            username="JohnSmith",
+            password="JSPassword"
+        )
+        self.user_a.save()
+
+        self.user_b = User.objects.create_user(
+            first_name="Jane",
+            last_name="Doe",
+            email="jane@doe.com",
+            username="JaneDoe",
+            password="JDPassword"
+        )
+        self.user_b.save()
+
+        self.post = Post.objects.create(
+            author=self.user_a,
+            title="Test post",
+            content="Test post content",
+            status=1,
+            post_type=1
+        )
+        self.post.save()
+
+        self.categories = Category(
+            name="Miscellaneous",
+            label_colour="#000000" 
+        )
+        self.categories.save()
+
+    def test_user_can_edit_own_post(self):
+        """
+        Test that a logged in user can edit their own posts
+        """
+        self.client.login(username="JohnSmith", password="JSPassword")
+        data = {
+            "title": "Test Post",
+            "content": "Change post content",
+            "categories": [1]
+        }
+        response = self.client.post(reverse(
+            'edit_post', args=["test-post"]), data, follow=True)
+        self.assertRedirects(
+            response,
+            '/test-post/' 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            Post.objects.filter(content="Change post content").exists())
+
+        
