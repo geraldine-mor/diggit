ac28033386b7247bf73a328c941b8c8248fc12f9
Geraldine <geraldine@rocketmal.com>
2026-05-09 11:11:23 +0100

    Add remaining test for views.py

--- blog/test_views.py
+++ blog/test_views.py
@@ -100,9 +100,8 @@ class TestForumListView(TestCase):
             'content': 'Test user generated content',
             'categories': [1]
         }
-        response = self.client.post(
-            "/diggit_forum/", data)
-       
+        response = self.client.post(reverse('diggit_forum'), data)
+            
         self.assertEqual(response.status_code, 200)
         self.assertFalse(
             Post.objects.filter(title="Logged Out Test").exists())
@@ -144,8 +143,8 @@ class TestReadPostView(TestCase):
         """
         Test that read_post returns 200 for a valid slug
         """
-        slug = "test-post"
-        response = self.client.get(reverse('read_post', args=[slug]))
+        response = self.client.get(
+            reverse('read_post',args=[self.post.slug]))
 
         self.assertEqual(response.status_code, 200)
 
@@ -167,10 +166,11 @@ class TestReadPostView(TestCase):
             'content': 'Test comment content',
         }
         response = self.client.post(
-            reverse('read_post', args=["test-post"]), data, follow=True)
+            reverse('read_post',
+                    args=[self.post.slug]), data, follow=True)
         self.assertRedirects(
             response,
-            '/test-post/' 
+            f"/{self.post.slug}/" 
         )
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Comment saved", response.content)
@@ -223,13 +223,243 @@ class TestEditPostView(TestCase):
             "categories": [1]
         }
         response = self.client.post(reverse(
-            'edit_post', args=["test-post"]), data, follow=True)
+            'edit_post', args=[self.post.slug]), data, follow=True)
         self.assertRedirects(
             response,
-            '/test-post/' 
+            f"/{self.post.slug}/" 
         )
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Post Updated!", response.content)
         self.assertTrue(
             Post.objects.filter(content="Change post content").exists())
 
+    def test_cannot_edit_not_other_users_post(self):
+        """
+        Test that a logged in user can't edit another user's posts
+        """
+        self.client.login(username="JaneDoe", password="JDPassword")
+        data = {
+            "title": "Test Post",
+            "content": "Change post content",
+            "categories": [1]
+        }
+        response = self.client.post(reverse(
+            'edit_post', args=[self.post.slug]), data, follow=True)
+        self.assertRedirects(
+            response,
+            '/diggit_forum/' 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"You can only edit your own posts.", response.content)
+        self.assertFalse(
+            Post.objects.filter(content="Change post content").exists())
         
+
+class TestDeletePostView(TestCase):
+    
+    def setUp(self):
+        self.user_a = User.objects.create_user(
+            first_name="John",
+            last_name="Smith",
+            email="john@smith.com",
+            username="JohnSmith",
+            password="JSPassword"
+        )
+        self.user_a.save()
+
+        self.user_b = User.objects.create_user(
+            first_name="Jane",
+            last_name="Doe",
+            email="jane@doe.com",
+            username="JaneDoe",
+            password="JDPassword"
+        )
+        self.user_b.save()
+
+        self.post = Post.objects.create(
+            author=self.user_a,
+            title="Test post",
+            content="Test post content",
+            status=1,
+            post_type=1
+        )
+        self.post.save()
+
+    def test_user_can_delete_own_post(self):
+        """
+        Test that logged in users can delete own posts
+        """
+        self.client.login(username="JohnSmith", password="JSPassword")
+        response = self.client.post(reverse(
+            'delete_post', args=[self.post.slug]), follow=True)
+        self.assertRedirects(
+            response,
+            '/diggit_forum/' 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Post deleted!", response.content)
+        self.assertFalse(
+            Post.objects.filter(title="Test post").exists())
+        
+    def test_user_b_cannot_delete_user_a_post(self):
+        """
+        Test that logged in users can't delete other user's posts
+        """
+        self.client.login(username="JaneDoe", password="JDPassword")
+        response = self.client.post(reverse(
+            'delete_post', args=[self.post.slug]), follow=True)
+        self.assertRedirects(
+            response,
+            '/diggit_forum/' 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"You cannot delete this post!", response.content)
+        self.assertTrue(
+            Post.objects.filter(title="Test post").exists())
+        
+
+class TestEditCommentView(TestCase):
+
+    def setUp(self):
+        self.user_a = User.objects.create_user(
+            first_name="John",
+            last_name="Smith",
+            email="john@smith.com",
+            username="JohnSmith",
+            password="JSPassword"
+        )
+        self.user_a.save()
+
+        self.user_b = User.objects.create_user(
+            first_name="Jane",
+            last_name="Doe",
+            email="jane@doe.com",
+            username="JaneDoe",
+            password="JDPassword"
+        )
+        self.user_b.save()
+
+        self.post = Post.objects.create(
+            author=self.user_a,
+            title="Test post",
+            content="Test post content",
+            status=1,
+            post_type=1
+        )
+        self.post.save()
+
+        self.comment = Comment.objects.create(
+            post=self.post,
+            author=self.user_b,
+            content="Test comment",    
+        )
+        self.comment.save()
+
+    def test_user_can_edit_own_comment(self):
+        """
+        Test that a logged in user can edit their own comments
+        """
+        self.client.login(username="JaneDoe", password="JDPassword")
+        data = {"content": "Change comment content"}
+        response = self.client.post(
+            reverse('edit_comment', args=[self.post.slug, self.comment.id]),
+            data, follow=True)
+        self.assertRedirects(
+            response,
+            f"/{self.post.slug}/" 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Comment Updated!", response.content)
+        self.assertTrue(
+            Comment.objects.filter(content="Change comment content").exists())
+
+    def test_cannot_edit_not_other_users_comment(self):
+        """
+        Test that a logged in user can't edit another user's comments
+        """
+        self.client.login(username="JohnSmith", password="JSPassword")
+        data = {"content": "Change comment content"}
+        response = self.client.post(reverse(
+            'edit_comment', args=[self.post.slug, self.comment.id]),
+            data, follow=True)
+        self.assertRedirects(
+            response,
+            f"/{self.post.slug}/" 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"You can only edit your own comments.", response.content)
+        self.assertFalse(
+            Comment.objects.filter(content="Change comment content").exists())
+
+
+class TestDeleteCommentView(TestCase):
+    
+    def setUp(self):
+        self.user_a = User.objects.create_user(
+            first_name="John",
+            last_name="Smith",
+            email="john@smith.com",
+            username="JohnSmith",
+            password="JSPassword"
+        )
+        self.user_a.save()
+
+        self.user_b = User.objects.create_user(
+            first_name="Jane",
+            last_name="Doe",
+            email="jane@doe.com",
+            username="JaneDoe",
+            password="JDPassword"
+        )
+        self.user_b.save()
+
+        self.post = Post.objects.create(
+            author=self.user_a,
+            title="Test post",
+            content="Test post content",
+            status=1,
+            post_type=1
+        )
+        self.post.save()
+
+        self.comment = Comment.objects.create(
+            post=self.post,
+            author=self.user_b,
+            content="Test comment",    
+        )
+        self.comment.save()
+
+    def test_user_can_delete_own_comment(self):
+        """
+        Test that logged in users can delete own comments
+        """
+        self.client.login(username="JaneDoe", password="JDPassword")
+        response = self.client.post(reverse(
+            'delete_comment', 
+            args=[self.post.slug, self.comment.id]), 
+            follow=True)
+        self.assertRedirects(
+            response,
+            f"/{self.post.slug}/" 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Comment deleted!", response.content)
+        self.assertFalse(
+            Comment.objects.filter(content="Test comment").exists())
+        
+    def test_user_a_cannot_delete_user_b_comment(self):
+        """
+        Test that logged in users can't delete other user's comments
+        """
+        self.client.login(username="JohnSmith", password="JSPassword")
+        response = self.client.post(reverse(
+            'delete_comment',
+            args=[self.post.slug, self.comment.id]), follow=True)
+        self.assertRedirects(
+            response,
+            f"/{self.post.slug}/" 
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"You cannot delete this comment!", response.content)
+        self.assertTrue(
+            Comment.objects.filter(content="Test comment").exists())
